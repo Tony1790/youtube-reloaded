6371471dafb80db3ace1fe705579c19309294551
Tony1790 <tony45081790@gmail.com>
2022-04-16 02:14:33 +0900

    0

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,19 @@
 import express from "express";
-import { remove, edit, see, logout } from "../controllers/usercontroller";
+import {
+  remove,
+  edit,
+  see,
+  logout,
+  startGithubLogin,
+  finishGithubLogin,
+} from "../controllers/usercontroller";
 
 const userRouter = express.Router();
 
 userRouter.get("/edit", edit);
 userRouter.get("/delete", remove);
 userRouter.get("/logout", logout);
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get("/:id", see);
 export default userRouter;
