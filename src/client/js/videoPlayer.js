3d79f94d267ea1e853f4a5f2d65a0644146fe441
Tony1790 <tony45081790@gmail.com>
2022-05-03 02:03:14 +0900

    11/04

---
+++ src/client/js/videoPlayer.js
@@ -0,0 +1,64 @@
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+
+let volumeValue = 0.5;
+video.volume = volumeValue;
+
+const handlePlayClick = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+  playBtn.innerText = video.paused ? "play" : "Pause";
+};
+
+const handleMuteBtn = (e) => {
+  if (video.muted) {
+    video.muted = false;
+    video.volume = volumeValue;
+  } else {
+    video.muted = true;
+    video.volume = 0;
+  }
+  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolumeChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  if (video.muted) {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+  volumeValue = value;
+  video.volume = value;
+
+  if (Number(value) === 0) {
+    video.muted = true;
+    muteBtn.innerText = "Unmute";
+  } else {
+    video.muted = false;
+    muteBtn.innerText = "Mute";
+  }
+};
+
+const handleLoadedMetaData = () => {
+  totalTime.innerText = Math.floor(video.duration);
+};
+
+const handleTimeUpdate = () => {
+  currentTime.innerText = Math.floor(video.currentTime);
+};
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMuteBtn);
+volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetaData);
+video.addEventListener("timeupdate", handleTimeUpdate);
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,10 @@ const path = require("path");
 console.log(path.resolve(__dirname, "assets", "js"));
 
 module.exports = {
-  entry: "./src/client/js/main.js",
+  entry: {
+    main: "./src/client/js/main.js",
+    videoPlayer: "./src/client/js/videoPlayer.js",
+  },
   mode: "development",
   watch: true, //heroku로 변경시 수정
   plugins: [
@@ -13,7 +16,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: "js/main.js",
+    filename: "js/[name].js",
     path: path.resolve(__dirname, "assets"),
     clean: true,
   },
