b6f794fefcfee83be19e6fb6b9de8aa8e29fda54
Tony1790 <tony45081790@gmail.com>
2022-05-07 16:37:04 +0900

    16 done

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,18 +1,40 @@
+import regeneratorRuntime from "regenerator-runtime";
+
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const handleSubmit = (event) => {
+const addComment = (text) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComments = document.createElement("li");
+  newComments.className = "video__comment";
+  const icon = document.createElement("i");
+  icon.className = "fas fa-comment";
+  const span = document.createElement("span");
+  span.innerText = ` ${text}`;
+  newComments.appendChild(icon);
+  newComments.appendChild(span);
+  videoComments.prepend(newComments);
+};
+
+const handleSubmit = async (event) => {
   event.preventDefault();
   const textarea = form.querySelector("textarea");
-  const videoId = videoContainer.dataset.id;
   const text = textarea.value;
-  fetch(`/api/videos/${videoId}/comments`, {
+  const videoId = videoContainer.dataset.id;
+  if (text === "" || text.trim() === "") {
+    return;
+  }
+  const { status } = await fetch(`/api/videos/${videoId}/comments`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ text }),
   });
+  if (status === 201) {
+    addComment(text);
+  }
+  textarea.value = "";
 };
 
 if (form) {
